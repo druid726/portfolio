c91d4d3efd9c3cdddf72935df5c4ffe881a756e4
druid726 <pdoneill26@gmail.com>
2015-06-29 21:38:21 -0400

    updated images

---
+++ footer.php
@@ -0,0 +1,34 @@
+ <?php
+ /**
+  * The template for displaying the footer
+  *
+  *
+  * Contains footer content and the closing of the #main and #page div elements.
+  *
+  * @package WordPress
+  * @subpackage Twenty_Fourteen
+  * @since PDO Webdev
+  * @author Patti O'Neill
+ */
+?>
+
+    </div><!-- #main -->
+
+    <footer class="site-footer" role="contentinfo">
+      <div class="site-info">
+        <p>&copy; PDO Web Development and Email Solutions | All Rights Reserved | &copy; 2015 </p>
+      </div><!-- .site-info -->
+
+      <nav class="social-media-navigation" role="navigation">
+        <div class="social-btns">
+          <a href="" class="soc-icon tw"></a>
+          <a href="" class="soc-icon fb"></a>
+          <a href="" class="soc-icon ln"></a>
+        </div>
+      </nav>
+    </footer>
+  </div><!-- #page -->
+
+  <?php wp_footer(); ?>
+</body>
+</html>
